d9ab0a9bd3f377c8e9f2dfe59d51262fed48a240
Noisegate <marcell.marosvolgyi@gmail.com>
2016-03-28 18:21:11 +0200

    refact + cleanup

--- analyzer.py
+++ analyzer.py
@@ -13,14 +13,28 @@ ser = serial.Serial(
     )
 
 
-class Constants(object):
+class Const(object):
 
     W = 640
     H = 480
 
+    margin = 10
+
+    rW = W-margin
+    rH = H-margin
+    r2W = rW-margin
+    r2H = rH-margin
+
+    halfW = W/2
+    halfH = H/2
+        
     #scaling
 
-    xs = 620.0/512.0
+    xs = r2W/512.0
+
+    @classmethod
+    def rescale(cls):
+        pass
 
 class Colors(object):
     red = pygame.Color(255, 0, 0)
@@ -44,19 +58,19 @@ class Axes(object):
         self.surface.blit(renderinstance, rectinstance)
 
     def draw(self):
-        pygame.draw.line(self.surface, Colors.darkred, (10,10),(630,10),2)
-        pygame.draw.line(self.surface, Colors.darkred, (630,10),(630,470),2)
-        pygame.draw.line(self.surface, Colors.darkred, (630,470),(10,470),2)
-        pygame.draw.line(self.surface, Colors.darkred, (10,470),(10,10),2)
+        pygame.draw.line(self.surface, Colors.darkred, (Const.margin,Const.margin), (Const.rW,Const.margin), 2)
+        pygame.draw.line(self.surface, Colors.darkred, (Const.rW,Const.margin), (Const.rW, Const.rH), 2)
+        pygame.draw.line(self.surface, Colors.darkred, (Const.rW, Const.rH), (Const.margin, Const.rH), 2)
+        pygame.draw.line(self.surface, Colors.darkred, (Const.margin,Const.rH), (Const.margin, Const.margin), 2)
         for i in range(10):
-            pygame.draw.line(self.surface, Colors.darkred, (i*64, 10),(i*64,470),1)
+            pygame.draw.line(self.surface, Colors.darkred, (i*Const.W/10, Const.margin),(i*Const.W/10,Const.rH), 1)
 
 class Main(object):
 
     def __init__(self):
         pygame.init()
         self.fps = pygame.time.Clock()
-        self.Surface = pygame.display.set_mode((640,480))
+        self.Surface = pygame.display.set_mode((Const.W, Const.H))
         pygame.display.set_caption("Teensy Analyser")
 
         self.red = pygame.Color(255, 0, 0)
@@ -66,6 +80,51 @@ class Main(object):
         self.axes = Axes(self.Surface)
 
         self.fourier = np.zeros(512, dtype=np.float)
+        self.waveform = np.zeros(512, dtype=np.float)
+
+    def plotfft(self):
+        yscale = 0.8 * Const.r2H/np.max(self.fourier)
+        offset = -10
+        for i in range(511):
+            pygame.draw.line(    self.Surface,
+                                Colors.red, 
+                                (int(10+Const.xs*i), Const.rH-yscale * self.fourier[i] + offset), 
+                                (int(10+Const.xs*(i+1)), Const.rH-yscale * self.fourier[i+1]+ offset))
+
+    def plotpeaks(self):
+        #http://stackoverflow.com/questions/4624970/finding-local-maxima-minima-with-numpy-in-a-1d-numpy-array
+        extrema = argrelextrema(self.fourier, np.greater)
+               
+        ea=extrema[0]
+        peaks = self.fourier[ea]
+        harm = 0.0
+        fund = 0.0
+
+        for i in range(len(peaks)):
+            pygame.draw.circle( self.Surface, 
+                                Colors.blue, 
+                                (   int(10+Const.xs*ea[i])+1, 
+                                     Const.r2H-int(1.0*Const.H*peaks[i])
+                                ), 
+                                2,
+                                0)    
+            if i==0:
+                fund = peaks[i]
+            else:
+                harm = harm + peaks[i]**2
+            try:
+                thd =  np.sqrt(harm)/(fund)*100
+            except:
+                thd = -1
+            self.axes.settext("THD: {0}%".format(thd), Const.halfW, 20, 16)
+            try:
+                self.axes.settext("fund: {0}kHz".format(ea[0]*44.032), 10, 40, 16)
+            except:
+                pass
+            for i in range(10):
+                self.axes.settext(" {0}k ".format(i/10.0*20), i*64, Const.rH, 8)
+
+        return thd                
 
     def loop(self):
         self.Surface.fill(Colors.black)
@@ -75,7 +134,7 @@ class Main(object):
             
             i=0
            
-            ser.write('?');
+            ser.write('?');#get the fourier data
             msg=[]
             while ser.inWaiting() > 0:
                 dummy = ser.read(1)
@@ -90,6 +149,7 @@ class Main(object):
                 #self.Bassbar.set(float(vals[0]))
                 preval =0
                 for i, val in enumerate(vals):
+                    #the first bytes are header with parameter info
                     if (i==0):
                         #freq
                         try:
@@ -100,49 +160,20 @@ class Main(object):
                         #print "frequency: {0}Hz\n".format(freq)
                         self.axes.settext("freq: {0}Hz".format(freq), 10, 10, 32)
 
-
+                    #the last 512 bytes are FFT data
                     elif (i<512):                   
                     #print val
                         try:
-                            y = int(float(val)*480)
+                            y = int(float(val)*Const.H)
                             self.fourier[i] = float(val)
-
-                            pygame.draw.line(   self.Surface, 
-                                                Colors.red, 
-                                                (int(10+Constants.xs*i), 460-preval), 
-                                                (int(10+Constants.xs*(i+1)), 460-y))
-                            preval = y
+                            #actual drawing somewhere else pls.
                         except:
-                            pass
-                #http://stackoverflow.com/questions/4624970/finding-local-maxima-minima-with-numpy-in-a-1d-numpy-array
-                extrema = argrelextrema(self.fourier, np.greater)
+                            self.fourier[i] = 0.0
                 
-                ea=extrema[0]
-                peaks = self.fourier[ea]
-                harm = 0.0
-                fund = 0.0
-                for i in range(len(peaks)):
-                     pygame.draw.circle(self.Surface, Colors.blue, (int(10+Constants.xs*ea[i])+1, 460-int(480.0*peaks[i])), 2,0)    
-                     if i==0:
-                        fund = peaks[i]
-                     else:
-                        harm = harm + peaks[i]**2
-                
-                try:
-                    thd =  np.sqrt(harm)/(fund)*100
-                except:
-                    thd = -1
-                self.axes.settext("THD: {0}%".format(thd), 320, 20, 16)
-                try:
-                    self.axes.settext("fund: {0}kHz".format(ea[0]*44.032), 10, 40, 16)
-                except:
-                    pass
-                for i in range(10):
-                    self.axes.settext(" {0}k ".format(i/10.0*20), i*64,470, 8)
+                self.plotfft()
+                thd = self.plotpeaks()
 
                 pygame.display.update()
-
-                
 
             for event in pygame.event.get():
                 if event.type == QUIT:
