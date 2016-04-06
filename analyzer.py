f28cc10547e677ff1870942f893b741a9bd7f2d0
Noisegate <marcell.marosvolgyi@gmail.com>
2016-04-06 17:20:15 +0200

    modified:   analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -6,6 +6,7 @@ import numpy as np
 from scipy.signal import argrelextrema
 import time
 import socket
+import subprocess
 
 ser = serial.Serial(
     port='/dev/ttyACM0',
@@ -20,6 +21,58 @@ MODE_SWEEP = 2
 MODE_THD = 4
 MODE_RMS = 3
 
+class Gnuplotter(object):
+    
+    script = """set logscale x 10
+                set logscale y 10
+                set grid mxtics mytics xtics ytics lt 1 lc rgb 'gray70', lt 1 lc rgb 'gray90'
+                set mxtics 10
+                set mytics 5
+
+                set boxwidth 8
+
+                set title "{title}" 
+                set xlabel "freq[Hz]"
+                set ylabel "amp[rel.]"
+
+                set xrange [10:50000]
+                set yrange [0.001: 1]
+                
+                plot {plot}
+
+                set size 1.0, 1.0
+                #set terminal postscript portrait enhanced mono dashed lw 1 "Helvetica" 14 
+                #set terminal pngcairo  transparent enhanced font "arial,10" fontscale 1.0 size 500,500; set zeroaxis;
+                #set terminal pngcairo  enhanced font "arial,20" fontscale 1.0 size 1500,1500; 
+                #set output "my-plot.png"
+                #set terminal pdf
+                #set output "my-plot.pdf"
+                set term svg enhanced font "arial,20" mouse size 1500,1200
+                set output "./tmp/my-plot.svg"
+                replot
+                set terminal x11
+                set size 1,1
+
+                #pause -1
+            """
+
+    def __init__(self, plotfiles):
+        myfile = open("./tmp/gnuplot.scr","w")
+        string = ''
+        for name in plotfiles:
+            string += '"{name}" using 1:2 smooth csplines title "{title}" with lines,'.format(name=name, title=name) 
+        
+        script = self.script.format(
+                    title = 'frequency sweep',
+                    plot =  string
+                )   
+        
+        myfile.write(script)
+        myfile.close()
+
+    def plot(self):
+        subprocess.Popen("gnuplot ./tmp/gnuplot.scr", shell=True, stdout=subprocess.PIPE).stdout.read()
+ 
 class Const(object):
 
     #W = 1280#640
@@ -98,7 +151,10 @@ class Handleargs(object):
              sys.exit(2)
         for opt, arg in opts:
             if opt == '-h':
-                print "analyzer.py -d <device>; device = raspi, laptop, desktop"
+                f=open('help.txt', 'r')
+                for line in f.readlines():
+                    print line,
+                f.close()
                 sys.exit()
             if opt == '-d':
                 self.device = arg
@@ -111,38 +167,44 @@ class Handleargs(object):
                 if arg == 'window':
                     Const.window()
 
-class  Axes(object):
+class Axes(object):
 
     def __init__(self, surface):
         self.surface = surface
         
-    def settext(self, text, x, y, size):
+    def settext(self, text, x, y, size,center):
         fontinstance = pygame.font.Font('freesansbold.ttf', size)
 
         renderinstance = fontinstance.render(text, False, Colors.blue)
         rectinstance = renderinstance.get_rect()
-        rectinstance.topleft=(x, y)
+
+        if (center):
+            rectinstance.centerx = x
+            rectinstance.centery = y
+        else:
+            rectinstance.topleft=(x, y)
         self.surface.blit(renderinstance, rectinstance)
 
-    def draw(self):
+    def frame(self):
         pygame.draw.line(self.surface, Colors.darkred, (Const.margin,Const.margin), (Const.rW,Const.margin), 2)
         pygame.draw.line(self.surface, Colors.darkred, (Const.rW,Const.margin), (Const.rW, Const.rH), 2)
         pygame.draw.line(self.surface, Colors.darkred, (Const.rW, Const.rH), (Const.margin, Const.rH), 2)
         pygame.draw.line(self.surface, Colors.darkred, (Const.margin,Const.rH), (Const.margin, Const.margin), 2)
+
+
+    def draw(self):
+        self.frame()
         for i in range(10):
             pygame.draw.line(self.surface, Colors.darkred, (i*Const.W/10, Const.margin),(i*Const.W/10,Const.rH), 1)
 
     def drawlog(self):
-        pygame.draw.line(self.surface, Colors.darkred, (Const.margin,Const.margin), (Const.rW,Const.margin), 2)
-        pygame.draw.line(self.surface, Colors.darkred, (Const.rW,Const.margin), (Const.rW, Const.rH), 2)
-        pygame.draw.line(self.surface, Colors.darkred, (Const.rW, Const.rH), (Const.margin, Const.rH), 2)
-        pygame.draw.line(self.surface, Colors.darkred, (Const.margin,Const.rH), (Const.margin, Const.margin), 2)
-        self.settext('ip {0}'.format(Const.ip),1100,10,18)
+        self.frame()
+        self.settext('ip {0}'.format(Const.ip),1100,10,18, False)
         logrange =  [1, 10.0,100.0,1000.0,10000.0]
 
         for i in [0,1,2,3]:
             currX = int(np.log10(logrange[i])*Const.r2W/4.0)+Const.margin
-            self.settext(" {0}Hz ".format(logrange[i+1]), currX, Const.rH, 10)
+            self.settext(" {0}Hz ".format(logrange[i+1]), currX, Const.rH, 10, False)
  
             for j in np.arange(logrange[i], logrange[i+1], logrange[i], dtype=np.float):
                 
@@ -160,6 +222,21 @@ class  Axes(object):
 
 
 
+class Stats(object):
+
+    def __init__(self, wav):
+        self.wav = wav
+        self.max = None
+        self.min = None
+        self.rms = None
+        self.std = None
+
+        self.dostats()
+
+    def dostats(self):
+        self.max = np.nanmax(self.wav)        
+        self.min = np.nanmin(self.wav)
+        self.std = np.nanstd(self.wav)
 
 class Main(object):
 
@@ -200,7 +277,7 @@ class Main(object):
         yscale = 0.8 * Const.r2H/np.max(self.fourier)
         return yscale
 
-    def plotsweep(self, points):
+    def plotsweep(self, points, scale):
         #points should be less than 2000
         yscale= Const.r2H/1.0;
         xscale= Const.r2W/4;
@@ -208,7 +285,10 @@ class Main(object):
         xoffset=xscale
         self.Surface.fill(Colors.background)
         self.axes.drawlog()
-        self.axes.settext("Sweep mode", 10, 10, 32)
+        self.axes.settext("Sweep mode", 10, 10, 32, False)
+
+        if scale:
+            pass
 
         for i in range(points):
             if i>2:
@@ -236,10 +316,13 @@ class Main(object):
         xscale = 1.0 * Const.r2W/bins
 
         for i in range(bins-1):
-            pygame.draw.line(    self.Surface,
-                                Colors.red, 
-                                (int(10+xscale*i), Const.rH-yscale * self.fourier[i] + offset), 
-                                (int(10+xscale*(i+1)), Const.rH-yscale * self.fourier[i+1]+ offset))
+            try:
+                pygame.draw.line(    self.Surface,
+                                    Colors.red, 
+                                    (int(10+xscale*i), Const.rH-yscale * self.fourier[i] + offset), 
+                                    (int(10+xscale*(i+1)), Const.rH-yscale * self.fourier[i+1]+ offset))
+            except:
+                pass
 
         if withpeaks:
             #http://stackoverflow.com/questions/4624970/finding-local-maxima-minima-with-numpy-in-a-1d-numpy-array
@@ -266,19 +349,20 @@ class Main(object):
                     thd =  np.sqrt(harm)/(fund)*100
                 except:
                     thd = -1
-                self.axes.settext("THD: {0}%".format(thd), Const.halfW, 20, 16)
+                self.axes.settext("THD: {0}%".format(thd), Const.halfW, 20, 16, False)
                 try:
-                    self.axes.settext("fund: {0}kHz".format(ea[0]*44.032), 10, 40, 16)
+                    self.axes.settext("fund: {0}kHz".format(ea[0]*44.032), 10, 40, 16, False)
                 except:
                     pass
                 for i in range(10):
-                    self.axes.settext(" {0}k ".format(i/10.0*20), i*64, Const.rH, 10)
+                    self.axes.settext(" {0}k ".format(i/10.0*20), i*64, Const.rH, 10, False)
     
             return thd                
 
     def rms(self):
         i=0
         self.Surface.fill(Colors.background)
+        self.axes.frame()
         ser.write('?')
         msg =[]
         while ser.inWaiting() > 0:
@@ -288,6 +372,7 @@ class Main(object):
         
         if (i>0):
             self.Surface.fill(Colors.background)
+            self.axes.frame()
             msgstr = ''.join(msg)
             vals = msgstr.split(':')
             for i, val in enumerate(vals):
@@ -299,7 +384,7 @@ class Main(object):
                 except:
                     rms = -1
                     freq=-1
-            self.axes.settext("RMS @ {0:04d}Hz = {1:.2f}V".format(int(freq), rms), 100,100,32)
+            self.axes.settext("RMS @ {0:04d}Hz = {1:.2f}V".format(int(freq), rms), Const.halfW ,Const.halfH, 32, True)
         pygame.display.update()
 
     def fft(self):
@@ -328,13 +413,13 @@ class Main(object):
                         freq=-1
     
                     #print "frequency: {0}Hz\n".format(freq)
-                    self.axes.settext("freq: {0}Hz".format(freq), 10, 10, 32)
+                    self.axes.settext("freq: {0}Hz".format(freq), 10, 10, 32, False)
                 elif (i==1):
                     try:
                         vol = float(val)
                     except:
                         vol = -1
-                    self.axes.settext("level: {0}".format(vol), 300,10,32)    
+                    self.axes.settext("level: {0}".format(vol), 300,10,32, False)    
                 #the last 512 bytes are FFT data
                 elif (i<512):                   
                 #print val
@@ -348,7 +433,7 @@ class Main(object):
             pygame.display.update()
 
     def sweep(self): 
-        N=1024
+        N=128
         s=''
         for i, fr in enumerate(np.logspace(1.3, 4.3, N)):
             ser.write("?{0}".format(int(fr)))
@@ -369,37 +454,39 @@ class Main(object):
             except:
                 pass
             if (i & 10 == 0):
-                self.plotsweep(i)
+                self.plotsweep(i, False)
             s = self.eventhandler()
             if s == 'f':
                 break
             if s == 'r':
                 break
 
-            if s=='':
-               file = open('sweep.dat', 'w')
-               for i, j in enumerate(self.waveformx):
-                   file.write("{0} {1}\r\n".format(j, self.calibrate[i]*self.waveformy[i]))
-               file.close()
-               self.axes.settext("select: s=sweep, c=save calibration, f=back to fft mode", 100, 600, 32)
-	       pygame.display.update()
-	       s = self.waitforkey()
+        stats = Stats(self.waveformy)
+        print stats.max
+        print stats.min
+        print stats.std
 
-               if (s=='c'):
-                   file = open('cal.dat', 'w')
-                   for i, j  in enumerate(self.waveformx):
-                       if self.waveformy[i]!=0.0:
-                           self.calibrate[i] = 1.0/self.waveformy[i]
-                       else:
-                           self.calibrate[i]=1.0
-                       file.write("{0} {1}\r\n".format(j, self.calibrate[i]))
-                   file.close()
- 
-               if (s=='f'):
-                   self.modeofoperation = MODE_FFT
-                   ser.write('f')
-                   time.sleep(2)
+        if s=='':
+            file = open('./tmp/sweep.dat', 'w')
+            for i, j in enumerate(self.waveformx):
+               file.write("{0} {1}\r\n".format(j, self.calibrate[i]*self.waveformy[i]))
+            file.close()
+            self.axes.settext("select: s=sweep, c=save calibration, f=back to fft mode", Const.halfW, Const.r2H, 16, True)
+            pygame.display.update()
+            plotje = Gnuplotter(['./tmp/sweep.dat'])
+            plotje.plot()
+            while s=='':
+                s = self.eventhandler()
 
+        if (s=='c'):
+            file = open('cal.dat', 'w')
+            for i, j  in enumerate(self.waveformx):
+                if self.waveformy[i]!=0.0:
+                    self.calibrate[i] = 1.0/self.waveformy[i]
+                else:
+                    self.calibrate[i]=1.0
+                file.write("{0} {1}\r\n".format(j, self.calibrate[i]))
+            file.close()
 
     def eventhandler(self):
         for event in pygame.event.get():
@@ -429,25 +516,12 @@ class Main(object):
                     ser.write('f')
                     self.modeofoperation = MODE_FFT
                     return 'f'
-
-    def waitforkey(self):
-        while True:
-            for event in pygame.event.get():
-                if event.type == QUIT:
-                    pygame.quit()
-                    sys.exit()
-                if event.type == KEYDOWN:
-                    if event.key == K_c:
-                        return 'c'
-                    if event.key == K_f:
-                        return 'f' 		
-                    if event.key == K_s:
-                        return 's'
+        return ''
 
     def loop(self):
         self.Surface.fill(Colors.black)
         self.axes.draw()
-        self.axes.settext("Initializing...", 10,10,32)
+        self.axes.settext("Initializing...", 10,10,32, False)
         pygame.display.update()
 
         ser.write('f')#make sure we are in default FFT modus
